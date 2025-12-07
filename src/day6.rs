5c8f6fe04a3786aaf63eab61ad6503442ddf1cfd
Jean-Marc Saffroy <saffroy@gmail.com>
2025-12-07 03:18:21 +0100

    day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -33,8 +33,40 @@ pub fn parse_1(text: &str) -> i64 {
         .sum()
 }
 
-pub fn parse_2(_text: &str) -> i64 {
-    0
+pub fn parse_2(text: &str) -> i64 {
+    let re = Regex::new(r"(\d+)\s*([*+]?)").unwrap();
+    let grid: Vec<_> = text.lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| line.as_bytes())
+        .collect();
+
+    let mut total = 0;
+    let mut args: Vec<i64> = vec![];
+
+    (0..grid[0].len())
+        .rev()
+        .for_each(|col| {
+            let v = (0..grid.len())
+                .map(|row| grid[row][col])
+                .collect::<Vec<u8>>();
+            let s = str::from_utf8(&v).unwrap();
+
+            if let Some(caps) = re.captures(s) {
+                args.push(caps[1].parse().unwrap());
+
+                if !caps[2].is_empty() {
+                    let inc = if &caps[2] == "*" {
+                        args.iter().product::<i64>()
+                    } else {
+                        args.iter().sum::<i64>()
+                    };
+                    total += inc;
+                    args.clear();
+                }
+            }
+        });
+
+    total
 }
 
 #[cfg(test)]
@@ -53,6 +85,6 @@ mod tests {
     }
     #[test]
     fn test6_parse2() {
-        assert_eq!(parse_2(&INPUT_TEXT_1), 6);
+        assert_eq!(parse_2(&INPUT_TEXT_1), 3263827);
     }
 }
